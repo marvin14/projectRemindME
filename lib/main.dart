5f5cbef19d9eb3d954652832ca3c0cbbbb003efa
marvin14 <ammersaver@gmail.com>
2019-11-19 15:57:35 +0800

    3rd commit with sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:project/pages/add_event_page.dart';
-import 'package:project/pages/add_task_page.dart';
+import 'package:project/pages/calendar.dart';
 import 'package:project/pages/event_page.dart';
+import 'package:project/pages/landing_page.dart';
 import 'package:project/pages/task_page.dart';
-import 'package:project/widgets/custom_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,204 +13,187 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RemindMe',
-      theme: ThemeData(
-      
-        primarySwatch: Colors.deepPurple
-      ),
-      home: MyHomePage(title: 'RemindMe'),
-      routes: <String,WidgetBuilder>{
-        "/taskPage":(BuildContext context)=>TaskPage(),
-        "/eventPage":(BuildContext context)=>EventPage()
-      }
-    );
+        title: 'RemindMe',
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        home: UserLogin(),
+        routes: <String, WidgetBuilder>{
+          "/taskPage": (BuildContext context) => TaskPage(),
+          "/eventPage": (BuildContext context) => EventPage(),
+          "/calendar": (BuildContext context) => HomePage(),
+          "/landing": (BuildContext context) => MyHomePage(),
+        });
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
+class UserLogin extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _UserLoginState createState() => _UserLoginState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+enum FormType { login, register }
 
-  PageController _pageController = PageController();
-  double currentPage = 0;
+class _UserLoginState extends State<UserLogin> {
+  TextEditingController emailEditingContrller = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  String _email;
+  String _password;
+  FormType _formType = FormType.login;
+
+  bool validate() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      Navigator.push(context, MaterialPageRoute(builder:(context)=>MyHomePage() ));
+      return true;
+    }
+    return false;
+  }
+
+  void validateSubmit() async {
+    if (validate()) {
+      try {
+        if (_formType == FormType.login){
+        FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password)) as FirebaseUser;
+        print('Signed in: ${user.uid}');
+      }else{
+        FirebaseUser user = (await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password)) as FirebaseUser;
+        print('Registered user:${user.uid}');      
+      }
+      } catch (e) {
+        print('Error:$e');
+      }
+    }
+  }
+  
+  void register() {
+   _formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void login(){
+    _formKey.currentState.reset();
+    setState(() {
+     _formType =FormType.login; 
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    _pageController.addListener((){
-      setState(() {
-       currentPage =_pageController.page; 
-      });
-    });
-
-      return Scaffold(
-        body:Stack(
-          children: <Widget>[_mainContext(context),
-          Positioned(
-            right:0,
-        child: Text("RM" ,style: TextStyle(fontSize: 200, color: Color(0x10000000)),)
-          ,)],
-        ),
-         
-         
-        floatingActionButton: FloatingActionButton(
-          onPressed:(){
-              showDialog(
-                barrierDismissible: false,
-                context: context,
-                builder: (BuildContext context){
-                  return Dialog(
-                    child: currentPage == 0 ? AddTaskPage() : AddEventPage(),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12)),);
-                }
-              );
-
-          },
-          child: Icon(Icons.add),
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-          
-          bottomNavigationBar:BottomAppBar(
-            color: Colors.grey[50],
-            shape: CircularNotchedRectangle(),
-            child:Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                IconButton(onPressed:(){},
-                icon: Icon(Icons.settings)
-                ,),
-              ],
-            ) 
-            ,) ,
-        );
-
-        }
-
-  Widget _mainContext(BuildContext context) {
-    var children2 = <Widget>[
-               SizedBox(height: 60),
-               Padding(
-                 padding:const EdgeInsets.all(24.0),
-                 
-                 
-               child:Text("Monday",
-                
-               style: TextStyle(fontSize: 35 ,
-               fontWeight: FontWeight.bold,
-               color: Colors.white,
-            )),
-               
-               ),
-  
-         
-            Row (
-                children: <Widget>[
-                Padding(padding: EdgeInsets.all(20.0),),
-                 Expanded(
-
-                       child: CustomButton(
-                          onPressed: (){
-                            Navigator.of(context).pushNamed("taskPage");
-                         _pageController.previousPage(
-                           duration:Duration(milliseconds: 500),
-                           curve:Curves.bounceInOut);
-                       },
-                      buttonText: "Task",
-                      
-                       color:
-                       currentPage < 0.5 ? Theme.of(context).accentColor:Colors.white,
-                       textColor:
-                        currentPage <0.5? Colors.black: Theme.of(context).accentColor,
-                      
-                   ),
-                     ),
-                 
-                 SizedBox(
-                   width: 32,
-                 ),
-
-                  Expanded(
-                     child: CustomButton(
-                         onPressed: (){
-                            Navigator.of(context).pushNamed("eventPage");
-                           _pageController.nextPage(
-                           duration:Duration(milliseconds: 500),
-                           curve:Curves.bounceInOut);
-                         },
-                     buttonText: "Events",
-
-                     color:
-                     currentPage > 0.5 ? Theme.of(context).accentColor:Colors.white,
-                     textColor:
-                        currentPage >0.5? Colors.black: Theme.of(context).accentColor,
-                     
-                   
-          
-                    ),
-                    ),
-                SizedBox(
-                    width:32 ,)
-               ],
-               ),
-               ];
-    return Column(
-        children: <Widget>[  
-
-        ClipPath (
-          clipper: Myclipper(),
-           child: Container(
-             child: Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: children2,
-               ),
-                
-            height:300 ,
-            decoration: BoxDecoration(
-            color:Colors.deepPurple[400] ),
-              ),
-                ),
-          
-        Expanded(
-        child: PageView(
-          controller: _pageController,
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        color: Theme.of(context).accentColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-          TaskPage(),EventPage()],
-        )),
-
-             
-             
-      ],
-     );
- 
+            SizedBox(
+              height: 80,
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            Text(
+              "RemindMe",
+              style: TextStyle(color: Colors.white, fontSize: 40,fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SingleChildScrollView(
+                        child: Container(
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: buildInputs()+submitButtons(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
+  List<Widget> buildInputs() {
+    return [
+      TextFormField(
+        validator: (value) => value.isEmpty ? 'Email Required' : null,
+        decoration: InputDecoration(
+          labelText: 'Email or Phone',
+        ),
+        onSaved: (value) => _email = value,
+      ),
+      TextFormField(
+        obscureText: true,
+        validator: (value) => value.isEmpty ? 'Password Required' : null,
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> submitButtons() {
+    if (_formType== FormType.login){
+    return [
+      RaisedButton(
+        onPressed: validateSubmit,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        color: Theme.of(context).accentColor,
+        child: Text(
+          "Login",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      FlatButton(
+        child: Text('Create account'),
+        onPressed: register,
+      )
+    ];
+  }else{
+     return [
+      RaisedButton(
+        onPressed: validateSubmit,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        color: Theme.of(context).accentColor,
+        child: Text(
+          "Create account",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      FlatButton(
+        child: Text('Have an account? Login'),
+        onPressed: login,
+      )
+     ];
+  }
 }
-
-   class Myclipper extends CustomClipper<Path>{
-  @override
-  Path getClip(Size size) {
-    var path =new Path();
-      path.lineTo(0, size.height-70);
-      var controllPoint=Offset(50,size.height);
-      var endPoint=Offset(size.width/2, size.height);
-      path.quadraticBezierTo(controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
-      path.lineTo(size.width, size.height);
-      path.lineTo(size.width, 0);
-      return path;    
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-  
-    return true;
-  }
-        
-      }
+}
---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,228 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:project/pages/add_event_page.dart';
+import 'package:project/pages/add_task_page.dart';
+import 'package:project/pages/calendar.dart';
+import 'package:project/pages/event_page.dart';
+import 'package:project/pages/task_page.dart';
+import 'package:project/widgets/custom_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class MyHomePage extends StatefulWidget {
+ 
+  const MyHomePage({Key key, this.user, this.title}) : super(key: key);
+  final FirebaseUser user;
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  PageController _pageController = PageController();
+  double currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    _pageController.addListener((){
+      setState(() {
+       currentPage =_pageController.page; 
+      });
+    });
+    
+      return Scaffold(
+        appBar: AppBar(title: Text(""),),
+        drawer: Drawer(child: ListView(children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text ("Bonani Marvin"), 
+            accountEmail: Text("marvinbonani14@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor:Colors.white ,
+              child: Text("M"),),
+            ),
+
+        
+          ListTile(title: Text("Profile"),
+          trailing: Icon(Icons.arrow_forward),
+
+          
+          ),
+          Divider(),
+           ListTile
+           (title: Text("Calendar"),
+          trailing: Icon(Icons.arrow_forward),
+          onTap:()=>Navigator.of(context).pushNamed("/calendar") ,
+          ),
+          Divider(),        
+           ListTile(title: Text("Settings"),
+          trailing: Icon(Icons.settings),
+          
+          ),
+
+        ],),),
+        body:Stack(
+          children: <Widget>[_mainContext(context),
+          Positioned(
+            right:0,
+        child: Text("RM" ,style: TextStyle(fontSize: 200, color: Color(0x10000000)),)
+          ,)],
+        ),
+         
+         
+        floatingActionButton: FloatingActionButton(
+          onPressed:(){
+              showDialog(
+                barrierDismissible: false,
+                context: context,
+                builder: (BuildContext context){
+                  return Dialog(
+                    child: currentPage == 0 ? AddTaskPage() : AddEventPage(),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)),);
+                }
+              );
+
+          },
+          child: Icon(Icons.add),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          
+          bottomNavigationBar:BottomAppBar(
+            color: Colors.grey[50],
+            shape: CircularNotchedRectangle(),
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(onPressed:(){},
+                icon: Icon(Icons.settings)
+                ,),
+              ],
+            ) 
+            ,) ,
+        );
+
+        }
+
+  Widget _mainContext(BuildContext context) {
+    var children2 = <Widget>[
+               SizedBox(height: 60),
+               Padding(
+                 padding:const EdgeInsets.all(24.0),
+                 
+                 
+               child:Text("Monday",
+                
+               style: TextStyle(fontSize: 35 ,
+               fontWeight: FontWeight.bold,
+               color: Colors.white,
+            )),
+               
+               ),
+  
+         
+            Row (
+                children: <Widget>[
+                Padding(padding: EdgeInsets.all(20.0),),
+                 Expanded(
+
+                       child: CustomButton(
+                          onPressed: (){
+                            Navigator.of(context).pushNamed("taskPage");
+                         _pageController.previousPage(
+                           duration:Duration(milliseconds: 500),
+                           curve:Curves.bounceInOut);
+                       },
+                      buttonText: "Task",
+                      
+                       color:
+                       currentPage < 0.5 ? Theme.of(context).accentColor:Colors.white,
+                       textColor:
+                        currentPage <0.5? Colors.black: Theme.of(context).accentColor,
+                      
+                   ),
+                     ),
+                 
+                 SizedBox(
+                   width: 32,
+                 ),
+
+                  Expanded(
+                     child: CustomButton(
+                         onPressed: (){
+                            Navigator.of(context).pushNamed("eventPage");
+                           _pageController.nextPage(
+                           duration:Duration(milliseconds: 500),
+                           curve:Curves.bounceInOut);
+                         },
+                     buttonText: "Events",
+
+                     color:
+                     currentPage > 0.5 ? Theme.of(context).accentColor:Colors.white,
+                     textColor:
+                        currentPage >0.5? Colors.black: Theme.of(context).accentColor,
+                     
+                   
+          
+                    ),
+                    ),
+                SizedBox(
+                    width:32 ,)
+               ],
+               ),
+               ];
+    return Column(
+        children: <Widget>[  
+
+        ClipPath (
+          clipper: Myclipper(),
+           child: Container(
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: children2,
+               ),
+                
+            height:250 ,
+            decoration: BoxDecoration(
+            color:Colors.deepPurple[400] ),
+              ),
+                ),
+          
+        Expanded(
+        child: PageView(
+          controller: _pageController,
+          children: <Widget>[
+          TaskPage(),EventPage()],
+        )),
+
+             
+             
+      ],
+     );
+ 
+  }
+
+}
+
+   class Myclipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    var path =new Path();
+      path.lineTo(0, size.height-70);
+      var controllPoint=Offset(50,size.height);
+      var endPoint=Offset(size.width/2, size.height);
+      path.quadraticBezierTo(controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
+      path.lineTo(size.width, size.height);
+      path.lineTo(size.width, 0);
+      return path;    
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+  
+    return true;
+  }
+        
+      }
